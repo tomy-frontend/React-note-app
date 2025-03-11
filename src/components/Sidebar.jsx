9ddb7e72405de60bf78e50b70c050474621c529c
tomy.dev <xxx.d1231@gmail.com>
2025-03-11 09:30:16 +0900

    SidebarのCSS付与

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,8 +3,22 @@ import "./Sidebar.css";
 
 const Sidebar = () => {
   return (
-    <div className="Sidebar">
-      <h2>sidebar</h2>
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>ノート</h1>
+        <button>追加</button>
+      </div>
+
+      <div className="app-sidebar-notes">
+        <div className="app-sidebar-note">
+          <div className="sidebar-note-title">
+            <strong>タイトル</strong>
+            <button>削除</button>
+          </div>
+          <p>ノートの内容です。</p>
+          <small>最後の修正日:2025/03/11</small>
+        </div>
+      </div>
     </div>
   );
 };
