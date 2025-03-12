0beddb78578675a0c1d9960c88c262fc2a288351
tomy.dev <xxx.d1231@gmail.com>
2025-03-12 22:11:02 +0900

    filter関数を使用して削除ボタンの実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
+import uuid from "react-uuid";
 
 function App() {
   const [notes, setNotes] = useState([]); // 作成したnoteを保存する配列
@@ -10,7 +11,7 @@ function App() {
   const onAddNote = () => {
     console.log("noteが作成されました。");
     const newNote = {
-      id: 1,
+      id: uuid(), // これだけでランダムなidが発行される
       title: "新しいノート",
       content:
         "「吾輩は今日も縁側で昼寝をしながら、主人の苦々しい顔で論文を書いている様子を冷ややかに観察していた。書生は相変わらず無骨な様子で庭を掃いている。どうやら主人は近頃珍しく研究に没頭しているようだが、いかにも自惚れが強く見苦しい。その癖何も新しいことは書いていない。まったく人間というものは不可思議な生き物である。」",
@@ -21,9 +22,23 @@ function App() {
     console.log(notes);
   };
 
+  // 削除ボタンの処理
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id);
+    // filter関数は、このルールに合致したら"残す"、このルールに合致するものだけで新しい配列を作る。という考え方。
+    // useStateで管理しているnotes配列をフィルタリング、条件note.id !== idは一致するidを持つnote以外の全てに対して
+    // trueを返す。つまり配列の中に残るのは、押されなかったノート以外が配列に残る
+    // つまり、クリックされたノート以外で新しい配列を作成して、それをsetNotesで更新する処理。
+    setNotes(filterNotes);
+  };
+
   return (
     <div className="App">
-      <Sidebar onAddNote={onAddNote} notes={notes} />
+      <Sidebar
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        notes={notes}
+      />
       <Main />
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Sidebar.css";
 
-const Sidebar = ({ onAddNote, notes }) => {
+const Sidebar = ({ onAddNote, onDeleteNote, notes }) => {
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
@@ -15,7 +15,7 @@ const Sidebar = ({ onAddNote, notes }) => {
             <article key={note.id} className="app-sidebar-note">
               <div className="sidebar-note-title">
                 <strong>{note.title}</strong>
-                <button>削除</button>
+                <button onClick={() => onDeleteNote(note.id)}>削除</button>
               </div>
               <p>{note.content}</p>
               <small>
